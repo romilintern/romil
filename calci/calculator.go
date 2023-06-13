23250b9c63db1e524124e8befb6c2a17b02b166d
romilintern <romil.shah@senkusha.in>
2023-06-13 19:39:04 +0530

    romil here

---
+++ calci/calculator.go
@@ -0,0 +1,14 @@
+package calci
+
+func cal(cho int,a int b int) {
+ switch ch{
+ case "+":
+	fmt.println("Addition is",a+b)
+case "-":
+	fmt.println("Addition is",a-b)
+case "*":
+	fmt.println("Addition is",a*b)
+case "/":
+	fmt.println("Addition is",a/b)
+ }
+}
---
+++ program.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	fmt.Print("Enter first number: ")
+	fmt.Scanln(&a)
+	var b int
+	fmt.Print("Enter second number: ")
+	fmt.Scanln(&b)
+	var cho int
+	fmt.Print("Enter the choice for what you want to perform")
+
+	calci.cal()
+
+}
